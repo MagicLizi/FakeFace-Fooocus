4afc6069b280d1cbe583ac35a3d24514b57cd3d3
lizi <lizi@xd.com>
2024-04-19 17:38:29 +0800

    1

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import fooocus
 from typing import Annotated
 from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
-
+import json
+from urllib.parse import urlparse
+import os
 
 app = FastAPI()
 
@@ -15,6 +17,15 @@ app.add_middleware(
     allow_headers=["*"],  # 允许所有头
 )
 
+face_config = {}
+
+with open("./ff.json", 'r', encoding='utf-8') as file:
+    # 解析文件内容到Python数据结构
+    data = json.load(file)
+    for item in data:
+        face_config[item['name']] = item
+
+print(face_config)
 
 @app.get('/')
 async def root():
@@ -40,36 +51,63 @@ async def swap_face(paint_url: Annotated[str, Form()], mask_url: Annotated[str, 
     print(f"swap_face p {paint_url}")
     print(f"swap_face m {mask_url}")
     print(f"swap_face f {face_url}")
-    cnt = 1
-    result = fooocus.generate_in_paint_mode("", "copaxTimelessxlSDXL1_v11Lightning.safetensors",
-                                            "realisticStockPhoto_v20.safetensors",
-                                            0.6,
-                                            paint_url,
-                                            mask_url,
-                                            face_url,
-                                            0,
-                                            cnt)
-    print(result)
-    return {"code": 200, "data": {"list": result}}
+    parsed_url = urlparse(face_url)
+    path = parsed_url.path
+    face_name = os.path.basename(path)
+    cfg = face_config[face_name]
+    if cfg is not None:
+        cnt = 1
+        prompts = cfg['prompts']
+        base_model = cfg['model']
+        refiner_model = cfg['refiner_model']
+        print(prompts)
+        print(base_model)
+        print(refiner_model)
+        result = fooocus.generate_in_paint_mode(prompts, base_model,
+                                                refiner_model,
+                                                0.6,
+                                                paint_url,
+                                                mask_url,
+                                                face_url,
+                                                0,
+                                                cnt)
+        print(result)
+        return {"code": 200, "data": {"list": result}}
+    else:
+        return {"code": 500}
 
 
 @app.post("/detail")
 async def try_detail(paint_url: Annotated[str, Form()], mask_url: Annotated[str, Form()], face_url: Annotated[str, Form()], detail_type: Annotated[str, Form()]):
     print(f"detail p {paint_url}")
     print(f"detail m {mask_url}")
-    print(f"detail m {face_url}")
+    print(f"detail f {face_url}")
     print(f"detail detail_type {detail_type}")
-    cnt = 1
-    # detail_type 2 脸 3 手臂 4 腿 5 手 6 其他
-    result = fooocus.generate_in_paint_mode("real photo", "copaxTimelessxlSDXL1_v11Lightning.safetensors",
-                                            "realisticStockPhoto_v20.safetensors",
-                                            0.6,
-                                            paint_url,
-                                            mask_url,
-                                            face_url,
-                                            1,
-                                            cnt)
-    return {"code": 200, "data": {"list": result}}
+
+    parsed_url = urlparse(face_url)
+    path = parsed_url.path
+    face_name = os.path.basename(path)
+    cfg = face_config[face_name]
+    if cfg is not None:
+        cnt = 1
+        base_model = cfg['model']
+        refiner_model = cfg['refiner_model']
+        print(base_model)
+        print(refiner_model)
+        # detail_type 2 脸 3 手臂 4 腿 5 手 6 其他
+        result = fooocus.generate_in_paint_mode("real photo", base_model,
+                                                refiner_model,
+                                                0.6,
+                                                paint_url,
+                                                mask_url,
+                                                face_url,
+                                                1,
+                                                cnt)
+        print(result)
+        return {"code": 200, "data": {"list": result}}
+    else:
+        return {"code": 500}
+
 
 
 @app.post('/test')
