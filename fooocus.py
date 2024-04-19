cc27f2f83eb1040fdcb06b239f3e4f0b1ae07eb7
lizi <lizi@xd.com>
2024-04-19 21:41:32 +0800

    1

--- fooocus.py
+++ fooocus.py
@@ -1,14 +1,18 @@
 from gradio_client import Client
 import random
-import json
+import time
 import os
+import json
 import upyun_util
-
+from bs4 import BeautifulSoup
 client = Client("http://yiw1.dc.houdeyun.cn:64336/")
 # client = Client("http://127.0.0.1:7865/")
 
+deal_cache = {}
 
-def generate_in_paint_mode(prompts, base_model, refiner, refiner_weight, paint_url, mask_url, face_url, mode, cnt):
+
+def generate_in_paint_mode(prompts, base_model, refiner, refiner_weight, paint_url, mask_url, face_url, mode, cnt,
+                           key):
     print("some one gen ")
     if mode == 0:
         # default
@@ -23,11 +27,12 @@ def generate_in_paint_mode(prompts, base_model, refiner, refiner_weight, paint_u
 
     MIN_SEED = 0
     MAX_SEED = 2 ** 63 - 1
-    return generate(prompts, base_model, refiner, refiner_weight, paint_url, mask_url, face_url, in_paint_engine, in_paint_ds, in_paint_rf, str(random.randint(MIN_SEED, MAX_SEED)), cnt)
+    generate(prompts, base_model, refiner, refiner_weight, paint_url, mask_url, face_url, in_paint_engine, in_paint_ds,
+             in_paint_rf, str(random.randint(MIN_SEED, MAX_SEED)), cnt, key)
 
 
-def generate(prompts, base_model, refiner, refiner_weight, paint_url, mask_url, face_url, in_paint_engine, in_paint_ds, in_paint_rf, seed, cnt):
-
+def generate(prompts, base_model, refiner, refiner_weight, paint_url, mask_url, face_url, in_paint_engine, in_paint_ds,
+             in_paint_rf, seed, cnt, key):
     swap_face_str = "FaceSwap"
     stop_at = 0.9
     weight = 0.75
@@ -41,7 +46,8 @@ def generate(prompts, base_model, refiner, refiner_weight, paint_url, mask_url, 
     client.predict(
         False,  # bool in 'Generate Image Grid for Each Batch' Checkbox component
         prompts,  # str in 'parameter_11' Textbox component
-        "unrealistic, saturated, high contrast, big nose, painting, drawing, sketch, cartoon, anime, manga, render, CG, 3d, watermark, signature, label",  # str in 'Negative Prompt' Textbox component
+        "unrealistic, saturated, high contrast, big nose, painting, drawing, sketch, cartoon, anime, manga, render, CG, 3d, watermark, signature, label",
+        # str in 'Negative Prompt' Textbox component
         ["Fooocus V2", "Fooocus Photograph"],  # List[str] in 'Selected Styles' Checkboxgroup component
         "Speed",  # str in 'Performance' Radio component
         "704×1408 <span style='color: grey;'> ∣ 1:2</span>",  # str in 'Aspect Ratios' Radio component
@@ -51,23 +57,30 @@ def generate(prompts, base_model, refiner, refiner_weight, paint_url, mask_url, 
         False,  # bool in 'Read wildcards in order' Checkbox component
         2,  # int | float (numeric value between 0.0 and 30.0) in 'Image Sharpness' Slider component
         3,  # int | float (numeric value between 1.0 and 30.0) in 'Guidance Scale' Slider component
-        base_model,  # str (Option from: ['animaPencilXL_v100.safetensors', 'asianrealisticSdlife_v60.safetensors', 'copaxTimelessxlSDXL1_v11Lightning.safetensors', 'epicrealismXL_v5Ultimate.safetensors', 'iniverseMixXLSFWNSFW_74Real.safetensors', 'juggernautXL_v8Rundiffusion.safetensors', 'majicmixRealistic_v7.safetensors', 'newrealityxlAllInOne_30Experimental.safetensors', 'realisticStockPhoto_v20.safetensors', 'realvisxlV40_v40LightningBakedvae.safetensors']) in 'Base Model (SDXL only)' Dropdown component
-        refiner,  # str (Option from: ['None', 'animaPencilXL_v100.safetensors', 'asianrealisticSdlife_v60.safetensors', 'copaxTimelessxlSDXL1_v11Lightning.safetensors', 'epicrealismXL_v5Ultimate.safetensors', 'iniverseMixXLSFWNSFW_74Real.safetensors', 'juggernautXL_v8Rundiffusion.safetensors', 'majicmixRealistic_v7.safetensors', 'newrealityxlAllInOne_30Experimental.safetensors', 'realisticStockPhoto_v20.safetensors', 'realvisxlV40_v40LightningBakedvae.safetensors']) in 'Refiner (SDXL or SD 1.5)' Dropdown component
+        base_model,
+        # str (Option from: ['animaPencilXL_v100.safetensors', 'asianrealisticSdlife_v60.safetensors', 'copaxTimelessxlSDXL1_v11Lightning.safetensors', 'epicrealismXL_v5Ultimate.safetensors', 'iniverseMixXLSFWNSFW_74Real.safetensors', 'juggernautXL_v8Rundiffusion.safetensors', 'majicmixRealistic_v7.safetensors', 'newrealityxlAllInOne_30Experimental.safetensors', 'realisticStockPhoto_v20.safetensors', 'realvisxlV40_v40LightningBakedvae.safetensors']) in 'Base Model (SDXL only)' Dropdown component
+        refiner,
+        # str (Option from: ['None', 'animaPencilXL_v100.safetensors', 'asianrealisticSdlife_v60.safetensors', 'copaxTimelessxlSDXL1_v11Lightning.safetensors', 'epicrealismXL_v5Ultimate.safetensors', 'iniverseMixXLSFWNSFW_74Real.safetensors', 'juggernautXL_v8Rundiffusion.safetensors', 'majicmixRealistic_v7.safetensors', 'newrealityxlAllInOne_30Experimental.safetensors', 'realisticStockPhoto_v20.safetensors', 'realvisxlV40_v40LightningBakedvae.safetensors']) in 'Refiner (SDXL or SD 1.5)' Dropdown component
         refiner_weight,  # int | float (numeric value between 0.1 and 1.0) in 'Refiner Switch At' Slider component
         False,  # bool in 'Enable' Checkbox component
-        "None",  # str (Option from: ['None', 'sd_xl_offset_example-lora_1.0.safetensors', 'SDXL_FILM_PHOTOGRAPHY_STYLE_BetaV0.4.safetensors']) in 'LoRA 1' Dropdown component
+        "None",
+        # str (Option from: ['None', 'sd_xl_offset_example-lora_1.0.safetensors', 'SDXL_FILM_PHOTOGRAPHY_STYLE_BetaV0.4.safetensors']) in 'LoRA 1' Dropdown component
         - 2,  # int | float (numeric value between -2 and 2) in 'Weight' Slider component
         False,  # bool in 'Enable' Checkbox component
-        "None",  # str (Option from: ['None', 'sd_xl_offset_example-lora_1.0.safetensors', 'SDXL_FILM_PHOTOGRAPHY_STYLE_BetaV0.4.safetensors']) in 'LoRA 2' Dropdown component
+        "None",
+        # str (Option from: ['None', 'sd_xl_offset_example-lora_1.0.safetensors', 'SDXL_FILM_PHOTOGRAPHY_STYLE_BetaV0.4.safetensors']) in 'LoRA 2' Dropdown component
         - 2,  # int | float (numeric value between -2 and 2) in 'Weight' Slider component
         False,  # bool in 'Enable' Checkbox component
-        "None",  # str (Option from: ['None', 'sd_xl_offset_example-lora_1.0.safetensors', 'SDXL_FILM_PHOTOGRAPHY_STYLE_BetaV0.4.safetensors']) in 'LoRA 3' Dropdown component
+        "None",
+        # str (Option from: ['None', 'sd_xl_offset_example-lora_1.0.safetensors', 'SDXL_FILM_PHOTOGRAPHY_STYLE_BetaV0.4.safetensors']) in 'LoRA 3' Dropdown component
         - 2,  # int | float (numeric value between -2 and 2) in 'Weight' Slider component
         False,  # bool in 'Enable' Checkbox component
-        "None",  # str (Option from: ['None', 'sd_xl_offset_example-lora_1.0.safetensors', 'SDXL_FILM_PHOTOGRAPHY_STYLE_BetaV0.4.safetensors'])in 'LoRA 4' Dropdown component
+        "None",
+        # str (Option from: ['None', 'sd_xl_offset_example-lora_1.0.safetensors', 'SDXL_FILM_PHOTOGRAPHY_STYLE_BetaV0.4.safetensors'])in 'LoRA 4' Dropdown component
         - 2,  # int | float (numeric value between -2 and 2) in 'Weight' Slider component
         False,  # bool in 'Enable' Checkbox component
-        "None",  # str (Option from: ['None', 'sd_xl_offset_example-lora_1.0.safetensors', 'SDXL_FILM_PHOTOGRAPHY_STYLE_BetaV0.4.safetensors']) in 'LoRA 5' Dropdown component
+        "None",
+        # str (Option from: ['None', 'sd_xl_offset_example-lora_1.0.safetensors', 'SDXL_FILM_PHOTOGRAPHY_STYLE_BetaV0.4.safetensors']) in 'LoRA 5' Dropdown component
         - 2,  # int | float (numeric value between -2 and 2) in 'Weight' Slider component
         True,  # bool in 'Input Image' Checkbox component
         "inpaint",  # str in 'parameter_91' Textbox component
@@ -84,14 +97,21 @@ def generate(prompts, base_model, refiner, refiner_weight, paint_url, mask_url, 
         0.8,  # int | float (numeric value between 0.1 and 3.0) in 'Negative ADM Guidance Scaler' Slider component
         0.3,  # int | float (numeric value between 0.0 and 1.0) in 'ADM Guidance End At Step' Slider component
         7,  # int | float (numeric value between 1.0 and 30.0) in 'CFG Mimicking from TSNR' Slider component
-        "dpmpp_2m_sde_gpu",  # str (Option from: ['euler', 'euler_ancestral', 'heun', 'heunpp2', 'dpm_2', 'dpm_2_ancestral', 'lms', 'dpm_fast', 'dpm_adaptive', 'dpmpp_2s_ancestral', 'dpmpp_sde', 'dpmpp_sde_gpu', 'dpmpp_2m', 'dpmpp_2m_sde', 'dpmpp_2m_sde_gpu', 'dpmpp_3m_sde', 'dpmpp_3m_sde_gpu', 'ddpm', 'lcm', 'ddim', 'uni_pc', 'uni_pc_bh2']) in 'Sampler' Dropdown component
-        "karras",  # str (Option from: ['normal', 'karras', 'exponential', 'sgm_uniform', 'simple', 'ddim_uniform', 'lcm', 'turbo']) in 'Scheduler' Dropdown component
+        "dpmpp_2m_sde_gpu",
+        # str (Option from: ['euler', 'euler_ancestral', 'heun', 'heunpp2', 'dpm_2', 'dpm_2_ancestral', 'lms', 'dpm_fast', 'dpm_adaptive', 'dpmpp_2s_ancestral', 'dpmpp_sde', 'dpmpp_sde_gpu', 'dpmpp_2m', 'dpmpp_2m_sde', 'dpmpp_2m_sde_gpu', 'dpmpp_3m_sde', 'dpmpp_3m_sde_gpu', 'ddpm', 'lcm', 'ddim', 'uni_pc', 'uni_pc_bh2']) in 'Sampler' Dropdown component
+        "karras",
+        # str (Option from: ['normal', 'karras', 'exponential', 'sgm_uniform', 'simple', 'ddim_uniform', 'lcm', 'turbo']) in 'Scheduler' Dropdown component
         - 1,  # int | float (numeric value between -1 and 200) in 'Forced Overwrite of Sampling Step' Slider component
-        - 1,  # int | float (numeric value between -1 and 200) in 'Forced Overwrite of Refiner Switch Step' Slider component
-        - 1,  # int | float (numeric value between -1 and 2048) in 'Forced Overwrite of Generating Width' Slider component
-        - 1,  # int | float (numeric value between -1 and 2048) in 'Forced Overwrite of Generating Height' Slider component
-        - 1,  # int | float (numeric value between -1 and 1.0) in 'Forced Overwrite of Denoising Strength of "Vary"' Slider component
-        - 1,  # int | float (numeric value between -1 and 1.0) in 'Forced Overwrite of Denoising Strength of "Upscale"' Slider component
+        - 1,
+        # int | float (numeric value between -1 and 200) in 'Forced Overwrite of Refiner Switch Step' Slider component
+        - 1,
+        # int | float (numeric value between -1 and 2048) in 'Forced Overwrite of Generating Width' Slider component
+        - 1,
+        # int | float (numeric value between -1 and 2048) in 'Forced Overwrite of Generating Height' Slider component
+        - 1,
+        # int | float (numeric value between -1 and 1.0) in 'Forced Overwrite of Denoising Strength of "Vary"' Slider component
+        - 1,
+        # int | float (numeric value between -1 and 1.0) in 'Forced Overwrite of Denoising Strength of "Upscale"' Slider component
         False,  # bool in 'Mixing Image Prompt and Vary/Upscale' Checkbox component
         True,  # bool in 'Mixing Image Prompt and Inpaint' Checkbox component
         False,  # bool in 'Debug Preprocessors' Checkbox component
@@ -134,15 +154,40 @@ def generate(prompts, base_model, refiner, refiner_weight, paint_url, mask_url, 
         fn_index=40
     )
 
-    result = client.predict(fn_index=41)
-    full_path = os.path.join(result[3], "captions.json")
+    # result = client.predict(fn_index=41)
+
+    job = client.submit(fn_index=41)
+    while not job.done():
+        size = len(job.outputs())
+        if size > 0:
+            rst = job.outputs()[size - 1][0]
+            # print(rst)
+            htlm = rst['value']
+            soup = BeautifulSoup(htlm, 'lxml')
+            # 找到 progress 标签
+            progress = soup.find('progress')
+            # 获取 value 属性
+            value = progress.get('value') if progress else None
+            deal_cache[key] = {
+                "finish": False,
+                "progress": value,
+                "cnt":f"0/{cnt}"
+            }
+        time.sleep(1)
+
+    size = len(job.outputs())
+
+    full_path = os.path.join(job.outputs()[size - 1][3], "captions.json")
     with open(full_path, 'r') as file:
         file_content = file.read()
         data = json.loads(file_content)
         img_path = []
-        for key in data:
-            url = upyun_util.upload(key)
+        for k in data:
+            url = upyun_util.upload(k)
             img_path.append(url)
-        return img_path
 
+        deal_cache[key] = {
+            "finish": True,
+            "list": img_path
+        }
 
--- main.py
+++ main.py
@@ -6,7 +6,12 @@ from fastapi.middleware.cors import CORSMiddleware
 import json
 from urllib.parse import urlparse
 import os
-from fastapi.staticfiles import StaticFiles
+import threading
+import time
+import random
+import string
+
+length = 10
 
 app = FastAPI()
 
@@ -43,15 +48,26 @@ async def get_face_library(page: int = 1):
     else:
         rst_list = upyun_util.get_face_list("/fakeface/face")
         page_cnt = 30
-        total_page = int(len(rst_list)/page_cnt)
+        total_page = int(len(rst_list) / page_cnt)
         start = (page - 1) * page_cnt
         end = start + page_cnt
         sub_list = rst_list[start:end]
         return {"code": 200, "data": {"list": sub_list, "total_page": total_page}}
 
 
+@app.get("/key")
+async def get_key_cache(key: str = 1):
+    if key in fooocus.deal_cache:
+        rst = fooocus.deal_cache[key]
+        # print(rst)
+        return {"code": 200, "data": rst}
+    else:
+        return {"code": 500, "data": {}}
+
+
 @app.post("/swapface")
-async def swap_face(paint_url: Annotated[str, Form()], mask_url: Annotated[str, Form()], face_url: Annotated[str, Form()]):
+async def swap_face(paint_url: Annotated[str, Form()], mask_url: Annotated[str, Form()],
+                    face_url: Annotated[str, Form()]):
     print(f"swap_face p {paint_url}")
     print(f"swap_face m {mask_url}")
     print(f"swap_face f {face_url}")
@@ -60,29 +76,30 @@ async def swap_face(paint_url: Annotated[str, Form()], mask_url: Annotated[str, 
     face_name = os.path.basename(path)
     cfg = face_config[face_name]
     if cfg is not None:
-        cnt = 4
+        cnt = 2
         prompts = cfg['prompts']
         base_model = cfg['model']
         refiner_model = cfg['refiner_model']
         print(prompts)
         print(base_model)
         print(refiner_model)
-        result = fooocus.generate_in_paint_mode(prompts, base_model,
-                                                refiner_model,
-                                                0.6,
-                                                paint_url,
-                                                mask_url,
-                                                face_url,
-                                                0,
-                                                cnt)
-        print(result)
-        return {"code": 200, "data": {"list": result}}
+        # loop = asyncio.get_event_loop()
+
+        timestamp_ms = int(time.time() * 1000)
+        chars = string.ascii_letters
+        random_string = ''.join(random.choice(chars) for _ in range(length))
+        key = f'{timestamp_ms}{random_string}'
+        fooocus.deal_cache[key] = {"finish":False,"progress":0, "cnt":f"0/{cnt}"}
+        thread = threading.Thread(target=fooocus.generate_in_paint_mode, args=(prompts, base_model, refiner_model, 0.6, paint_url ,mask_url ,face_url, 0, cnt, key))
+        thread.start()
+        return {"code": 200, "data": {"key": key, "cnt":cnt}}
     else:
         return {"code": 500}
 
 
 @app.post("/detail")
-async def try_detail(paint_url: Annotated[str, Form()], mask_url: Annotated[str, Form()], face_url: Annotated[str, Form()], detail_type: Annotated[str, Form()]):
+async def try_detail(paint_url: Annotated[str, Form()], mask_url: Annotated[str, Form()],
+                     face_url: Annotated[str, Form()], detail_type: Annotated[str, Form()]):
     print(f"detail p {paint_url}")
     print(f"detail m {mask_url}")
     print(f"detail f {face_url}")
@@ -99,18 +116,24 @@ async def try_detail(paint_url: Annotated[str, Form()], mask_url: Annotated[str,
         print(base_model)
         print(refiner_model)
         # detail_type 2 脸 3 手臂 4 腿 5 手 6 其他
-        result = fooocus.generate_in_paint_mode("real photo", base_model,
-                                                refiner_model,
-                                                0.6,
-                                                paint_url,
-                                                mask_url,
-                                                face_url,
-                                                1,
-                                                cnt)
-        print(result)
-        return {"code": 200, "data": {"list": result}}
+        # result = fooocus.generate_in_paint_mode("real photo", base_model,
+        #                                         refiner_model,
+        #                                         0.6,
+        #                                         paint_url,
+        #                                         mask_url,
+        #                                         face_url,
+        #                                         1,
+        #                                         cnt)
+        # print(result)
+        # return {"code": 200, "data": {"list": result}}
+        timestamp_ms = int(time.time() * 1000)
+        chars = string.ascii_letters
+        random_string = ''.join(random.choice(chars) for _ in range(length))
+        key = f'{timestamp_ms}{random_string}'
+        fooocus.deal_cache[key] = {"finish": False, "progress":0,"cnt":f"0/{cnt}"}
+        thread = threading.Thread(target=fooocus.generate_in_paint_mode, args=(
+        "real photo", base_model, refiner_model, 0.6, paint_url, mask_url, face_url, 1, cnt, key))
+        thread.start()
+        return {"code": 200, "data": {"key": key,"cnt":cnt}}
     else:
         return {"code": 500}
-
-
-
