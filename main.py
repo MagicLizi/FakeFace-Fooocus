00b000f9ffcf7f786c78cc841809b6d2db8a0de2
lizi <lizi@xd.com>
2024-04-24 14:58:35 +0800

    1

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ async def swap_face_batch(face_url: Annotated[str, Form()], targets: Annotated[s
             client = rst['client']
             targets_list = json.loads(targets)
             keys = []
-            cnt = 1
+            cnt = 4
             for target in targets_list:
                 timestamp_ms = int(time.time() * 1000)
                 chars = string.ascii_letters
